37deb703399dff12d0509293da4bcb8b1af9a791
Eric Paulsen <ericpaulsen@coder.com>
2021-03-02 16:58:50 -0600

    added: functions & context

--- teardown.sh
+++ teardown.sh
@@ -1,6 +1,29 @@
 # !/usr/bin/env bash
+
+#/ NAME:
+#/ teardown.sh - Uninstalls Coder & deletes assocated resources, while keeping the-
+#/ namespace & cluster intact, in order to preserve a possible re-install.
+
 set -euo pipefail
 
-helm uninstall coder
+#/ Uninstalls all coder-related services in the cluster. 
+#/ Does not delete user environments or their assocated PVCs.
 
-kubectl get all,pvc,networkpolicy,secrets -l com.coder.resource=true -o name | xargs kubectl delete --force --grace-period=0
+function uninstall_coder() {
+    helm uninstall coder
+}
+
+#/ Gets all resources with the label of com.coder.resource
+#/ and deletes them from the cluster.
+
+function delete_resources() {
+    kubectl get all,pvc,networkpolicy,secrets -l com.coder.resource=true -o name | 
+    xargs kubectl delete --force --grace-period=0
+}
+
+function main() {
+    uninstall_coder
+    delete_resources
+}
+
+main
