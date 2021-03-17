524ad2807c4d6e606171b15a4a527671c67d370f
Eric Paulsen <ericpaulsen@coder.com>
2021-03-17 12:28:30 -0500

    add: --fix & automated re-install (#3)
    * add: --fix & automated re-install
    
    * add newlines
    
    * do not track tmp files
    
    * fix typo
    
    * only make dir if it doesn't exist
    
    * support missing VERSION for --fix
    
    * add validation to fix command
    
    Co-authored-by: Ben Potter <me@bpmct.net>

--- upgrade.sh
+++ upgrade.sh
@@ -7,51 +7,102 @@
 
 set -euo pipefail
 
-function prerequisites() {
-    # check if Coder helm repo is added, if not, add coder helm repo
-    if helm repo list | grep -q "https://helm.coder.com"; then
-       echo "Coder already exists within the current helm config."
-    else
-        helm repo add coder https://helm.coder.com
-    fi
-}
+echo "Enter the namespace your Coder deployment is in:"
 
-function upgrade() {
-# retrieve latest repository information
-    helm repo update
+read NAMESPACE
 
-# upgrade to desired version - if VERSION doesn't exist, 
-# run upgrade command with VERSION omitted (default to latest).
-   if [ "$VERSION" == "" ]; then
-        helm upgrade --namespace $NAMESPACE --force --install --atomic --wait \
-        coder coder/coder
-   else
-        helm upgrade --namespace $NAMESPACE --force --install --atomic --wait \
-        --version $VERSION coder coder/coder
-    fi
-}
+echo "Enter version you'd like to upgrade to"
 
-function usage() {
-    echo "Usage: set NAMESPACE and VERSION variables before running. 
-    VERSION format should follow x.xx.x. Omitting VERSION will default to latest."
-    exit 1
-}
+read VERSION
 
-# return usage if NAMESPACE is blank or VERSION doesn't match the correct format.
-    if [[ "$VERSION" != "" && ! "$VERSION" =~ ^([0-9]\.([1-9]|[1-9][0-9])\.[0-9])$ || "$NAMESPACE" == "" ]]; then
-        usage
-    fi
+# check if --fix flag exists. if so, then run --fix commands.
+if [ $# -ne 0 ]; then
+    while [ $# -ne 0 ]; do
+        case "$1" in
+            --fix)
+                echo "Attempting to fix your failed upgrade..."
+                echo "Exporting your helm chart values into a file > tmp/current-values.yml..."
+                mkdir -p tmp && helm get values --namespace $NAMESPACE coder > tmp/current-values.yml
+                echo "Uninstalling Coder..."
+                helm uninstall --namespace $NAMESPACE coder
+                echo "Waiting for all resources to delete..."
+                sleep 25
 
-function main() {
-    prerequisites
+                # reinstall to desired version - if VERSION doesn't exist, 
+                # run upgrade command with VERSION omitted (default to latest).
 
-    if upgrade; then
-        echo "Upgrade successful."
-    else
-        echo "Upgrade failed. Please reference our documentation for troubleshooting
-        a failed upgrade https://coder.com/docs/setup/updating#fixing-a-failed-upgrade. 
-        Alternatively, run ./upgrade.sh --fix to attempt the commands automatically."
-    fi
-}
+                function fix() {
+                    if [ "$VERSION" == "" ]; then
+                        helm upgrade --namespace $NAMESPACE --atomic \
+                        --wait --install --force coder coder/coder \
+                        --values tmp/current-values.yml
+                    else
+                        helm upgrade --namespace $NAMESPACE --atomic \
+                        --wait --install --force --version $VERSION \
+                        coder coder/coder --values tmp/current-values.yml   
+                    fi
+                }
 
-main
+                if fix; then
+                    echo "\n\nUpgrade successful."
+                else
+                    echo "\n\nUpgrade failed. Please reference our documentation for troubleshooting
+                    a failed upgrade https://coder.com/docs/setup/updating#fixing-a-failed-upgrade. 
+                    If you still run into issues, contact support@coder.com"
+                fi
+
+                exit >&2         
+        esac
+    done
+    # if flag exists, then do not run the below.
+    else {
+        function prerequisites() {
+        # check if Coder helm repo is added, if not, add coder helm repo
+            if helm repo list | grep -q "https://helm.coder.com"; then
+                echo "Coder already exists within the current helm config."
+            else
+                helm repo add coder https://helm.coder.com
+            fi
+                }
+
+        function upgrade() {
+        # retrieve latest repository information
+            helm repo update
+
+        # upgrade to desired version - if VERSION doesn't exist, 
+        # run upgrade command with VERSION omitted (default to latest).
+            if [ "$VERSION" == "" ]; then
+                helm upgrade --namespace $NAMESPACE --force --install --atomic --wait \
+                coder coder/coder
+            else
+                helm upgrade --namespace $NAMESPACE --force --install --atomic --wait \
+                --version $VERSION coder coder/coder
+            fi
+                }
+
+            function usage() {
+                echo "Usage: set NAMESPACE and VERSION variables before running. 
+                    VERSION format should follow x.xx.x. Omitting VERSION will default to latest."
+                exit 1
+                }
+
+            # return usage if NAMESPACE is blank or VERSION doesn't match the correct format.
+                if [[ "$VERSION" != "" && ! "$VERSION" =~ ^([0-9]\.([1-9]|[1-9][0-9])\.[0-9])$ || "$NAMESPACE" == "" ]]; then
+                    usage
+                fi
+
+            function main() {
+                prerequisites
+
+                if upgrade; then
+                    echo "\n\nUpgrade successful."
+                else
+                    echo "\n\nUpgrade failed. Please reference our documentation for troubleshooting
+                    a failed upgrade https://coder.com/docs/setup/updating#fixing-a-failed-upgrade. 
+                    Alternatively, run ./upgrade.sh --fix to attempt the commands automatically."
+                fi
+                }
+            main
+        }
+fi
+
