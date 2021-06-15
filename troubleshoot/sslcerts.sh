9749f945ad350e1555faf528bfee9468d0d3f86f
Eric Paulsen <ericpaulsen@coder.com>
2021-06-15 18:13:47 -0500

    feat: automate  kubectl outputs for cert-manager troubleshooting

---
+++ troubleshoot/sslcerts.sh
@@ -0,0 +1,42 @@
+# !/usr/bin/ev bash
+
+set -euo pipefail
+shopt -s expand_aliases
+
+FILE="coder-output.txt"
+alias k='kubectl'
+
+echo "Enter the namespace your Coder deployment is in:"
+
+read NAMESPACE
+
+echo "---" > $FILE
+echo "GET PODS" >> $FILE
+
+k get pods -n $NAMESPACE >> $FILE
+
+echo "---" >> $FILE
+echo "DESCRIBE INGRESS" >> $FILE
+
+k describe ingress -n $NAMESPACE >> $FILE
+
+echo "---" >> $FILE
+echo "GET CERT-MANAGER SECRETS" >> $FILE
+
+k get secrets -n cert-manager >> $FILE
+
+echo "---" >> $FILE
+
+CHECK=$(k get order,challenge)
+SUB='No resources'
+
+if [[ "$CHECK" == *"$SUB"* ]]; then
+    :
+else
+    echo "GET CHALLENGE & ORDER" >> $FILE
+    k describe challenge -A
+    k describe order -A
+fi
+
+
+
